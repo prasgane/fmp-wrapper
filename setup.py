43fd81b23626d562f194ea3f9e4cb226b09b094f
cccdenhart <cccdenhart@me.com>
2020-04-27 16:00:37 -0400

    adding files

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages, setup, Command
+
+# Package meta-data.
+NAME = 'financial-modeling-prep-wrapper'
+DESCRIPTION = 'A python wrapper for the Financial Modeling Prep API.'
+URL = 'https://github.com/me/myproject'
+EMAIL = 'me@example.com'
+AUTHOR = 'Awesome Soul'
+REQUIRES_PYTHON = '>=3.6.0'
+VERSION = '0.1.0'
+
+# What packages are required for this module to be executed?
+REQUIRED = [
+    # 'requests', 'maya', 'records',
+]
